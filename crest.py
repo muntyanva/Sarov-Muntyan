563d26af61ff04354a86b6b1c97772c073c15b82
VovaM <muntyan.sarov@gmail.com>
2021-02-13 13:41:08 +0300

    second version crest

--- crest.py
+++ crest.py
@@ -2,30 +2,32 @@ import pygame
 import sys
 
 data = list(map(str.strip, sys.stdin))
-w = int(data[0])
-h = int(data[1])
-
-if __name__ == '__main__':
+w = data[0]
+h = data[1]
+if type(w) == int and type(h) == int:
+    if __name__ == '__main__':
         # инициализация Pygame:
-    pygame.init()
+        pygame.init()
         # размеры окна:
-    size = width, height = w, h
+        size = width, height = w, h
         # screen — холст, на котором нужно рисовать:
-    screen = pygame.display.set_mode(size)
-    pygame.display.set_caption('Крест')
+        screen = pygame.display.set_mode(size)
+        pygame.display.set_caption('Крест')
         # формирование кадра:
         # команды рисования на холсте
-    pygame.draw.line(screen, (255, 255, 255), (0, 0), (w, h), width=5)
-    pygame.draw.line(screen, (255, 255, 255), (w, 0), (0, h), width=5)
-        # ...
+        pygame.draw.line(screen, (255, 255, 255), (0, 0), (w, h), width=5)
+        pygame.draw.line(screen, (255, 255, 255), (w, 0), (0, h), width=5)
+            # ...
         # ...
         # смена (отрисовка) кадра:
-    pygame.display.flip()
+        pygame.display.flip()
         # ожидание закрытия окна:
-    while pygame.event.wait().type != pygame.QUIT:
-        pass
+        while pygame.event.wait().type != pygame.QUIT:
+            pass
         # завершение работы:
-    pygame.quit()
+        pygame.quit()
+else:
+    print("Неправильный формат ввода")
 
 
 
